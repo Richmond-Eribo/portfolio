b143827079c78254ad8cccd3fcbb4550fac28941
Richmond Eribo <uyiosaeribo344@gmail.com>
2025-08-17 14:08:03 +0100

    chore: update dependencies and add new packages
    - Added several @radix-ui/react components for improved UI elements.
    - Updated lucide-react to version 0.539.0.
    - Added class-variance-authority, cmdk, next-themes, react-hook-form, sonner, tailwind-merge, and zod to dependencies.
    - Added tw-animate-css to devDependencies.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,11 +1,11 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function isActiveRoute(target: string, path: string) {
   // console.log(path, target, "checking", path.startsWith(target) ? true : false);
-  return target === path ? target === path : path.startsWith(target + "/");
+  return target === path ? target === path : path.startsWith(target + "/")
 }
