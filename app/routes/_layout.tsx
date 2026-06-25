82ff44ec263bc19bebbe4bb4a03827327626faa2
Richmond Eribo <uyiosaeribo344@gmail.com>
2026-06-25 19:10:05 +0100

    Updates layout route imports to use relative paths

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router"
-import { Header } from "@/components/header"
-import { Footer } from "@/components/footer"
+import { Header } from "../components/header"
+import { Footer } from "../components/footer"
+
 
 export default function Layout() {
   return (
